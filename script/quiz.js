222884041844a43c92fd52d93ef5735597b295f7
SeiPanda <70478113+SeiPanda@users.noreply.github.com>
2021-12-03 07:57:16 +0100

    menu close icon

--- script/quiz.js
+++ script/quiz.js
@@ -8,10 +8,20 @@ function handleClickLogo() {
 
 document.querySelector("#menu").addEventListener("click", loadMenu);
 
-
+let menuOpen = false;
 function loadMenu(){
     document.querySelector("#menu-container").classList.toggle("show");
-    document.querySelector("fas fa-bars").classList.remove("fa-bars").classList.add("fa-times")
+    if(menuOpen === true){   
+  
+        document.querySelector(".headerbar").classList.add("fa-bars");
+        document.querySelector(".headerbar").classList.remove("fa-times")
+        menuOpen=false;
+    }else{
+        
+        document.querySelector(".headerbar").classList.remove("fa-bars");
+        document.querySelector(".headerbar").classList.add("fa-times")
+        menuOpen=true;
+    }
 }
 
 let currentQuestion = 0;
